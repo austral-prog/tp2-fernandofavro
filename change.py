c1ca40b5887a34b826a416ff098ba19771f25eeb
fernandofavro <fjfavrovelo@mail.austral.edu.ar>
2025-08-19 03:08:37 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
 def change():
     expense = 23.75
     money = 100
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    vuelto = money - expense
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(int(vuelto))
+    print("Centavos:")
+    centavos = 0.25 * 100
+    print(int(centavos))
+change()
--- earth.py
+++ earth.py
@@ -1,3 +1,6 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    print("The result of X comes first in the dictionary than Y is " + str(x < y) + ".")
+    print("The result of Y comes first in the dictionary than X is " + str(y < x) + ".")
+earth()
