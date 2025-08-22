8574b14d28f9c724cfdbf71cb71a736b916c2294
fernandofavro <fjfavrovelo@mail.austral.edu.ar>
2025-08-22 16:13:33 -0300

    Add files via upload

--- ada.py
+++ ada.py
@@ -4,5 +4,5 @@ def ada():
     print(first_name.lower() + " " + last_name.lower())
     print(first_name.title() + " " + last_name.title())
     print(first_name.upper() + " " + last_name.upper())
-    print("   " + first_name.lower() + last_name.lower())
-#ada()
+     print("\t" + first_name.lower() + " " + last_name.lower())
+ada()
